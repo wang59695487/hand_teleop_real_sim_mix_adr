ba47ef088d7e62f5d1275201cc38240c13bc533c
wang59695487 <596954871@qq.com>
2023-11-02 14:22:23 -0700

    fix some bugs

--- hand_teleop/env/sim_env/pick_place_env.py
+++ hand_teleop/env/sim_env/pick_place_env.py
@@ -156,12 +156,12 @@ class PickPlaceEnv(BaseSimulationEnv):
                         0, len(COLOR_LIST))] * random.uniform(var, 1)
                     color[-1] = 1
                     mat.set_base_color(color)
+                    texture_path = get_shapenet_root_dir() / "textures" / \
+                        f"texture_{np.random.randint(0, 30)}.png"
+                    mat.set_diffuse_texture_from_file(str(texture_path))
                 mat.set_specular(random.uniform(0, var))
                 mat.set_roughness(random.uniform(0.7 - var, 0.7 + var))
                 mat.set_metallic(random.uniform(0, var))
-                texture_path = get_shapenet_root_dir() / "textures" / \
-                    f"texture_{np.random.randint(0, 30)}.png"
-                mat.set_diffuse_texture_from_file(str(texture_path))
                 geom.set_material(mat)
 
     def reset_env(self):
